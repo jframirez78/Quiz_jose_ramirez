8b101eef0cdf01f8e8100c7ec9b2a778241b4423
jframirez78 <jframirez78@misena.edu.co>
2020-12-04 22:26:42 -0500

    solucion del primer punto

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /*se crea index.js y se enlaza con index.html*/
+//primer punto//
+
+var TipoMotor = (5);
+switch (TipoMotor) {
+    case 0:
+        document.write ('No hay establecido un valor definido para el tipo debomba');
+            break;
+    case 1:
+        document.write ('La bomba es una bomba de agua');
+        break;
+    case 2:
+        document.write ('La bomba es una bomba de gasolina');
+        break;   
+    case 3:
+        document.write ('La bomba es una bomba de hormigón');
+        break;
+    case 4:
+        document.write ('La bomba es una bomba de pasta alimenticia');
+        break;
+    case 5:
+        document.write ('No existe un valor válido para tipo de bomba');
+        break;          
+}
+
+
